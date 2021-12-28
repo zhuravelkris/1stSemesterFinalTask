d6cb4f8046a501f8b3c6dbdf018f4ed2646f5968
zhuravel <kris.zhuravel@gmail.com>
2021-12-28 13:54:07 +0000

    C# Task decision added

---
+++ Task/Program.cs
@@ -0,0 +1,40 @@
+﻿// Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел. 
+// Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+int length = 20;
+int[] Array = new int[length];
+
+int CreateArray(int length)  //заполняем массив случайными числами и возвращаем количество четных чисел 
+{
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        Array[i] = new Random().Next(-50, 51);
+        if (Array[i] % 2 == 0) count++;
+    }
+    return count; 
+}
+
+int[] CreateEvenArray(int arg, int length)  //заполняем новый массив четными числами из предыдущего массива
+{
+    int[] evenNumbersArray = new int[arg];
+    for (int i = 0, j = 0; i < length; i++)
+    {
+        if (Array[i] % 2 == 0)
+        {
+            evenNumbersArray[j] = Array[i];
+            j++;
+        }
+    }
+    return evenNumbersArray;
+}
+
+int count = CreateArray(length);
+int[] evenNumbersArray = CreateEvenArray(count, length);
+
+Console.Clear();
+Console.WriteLine(String.Join(' ', Array));   //вывод в консоль первоначального массива
+Console.WriteLine(String.Join(' ', evenNumbersArray)); //вывод в консоль массива, состоящего из четных чисел
+
+
